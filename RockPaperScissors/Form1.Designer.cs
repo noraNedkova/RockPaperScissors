f72348caae53c7ec7fa330a78b7885ad226e21cb
noraNedkova <loranedeva3@gmail.com>
2025-11-11 22:22:24 +0200

    Project folder

---
+++ RockPaperScissors/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace RockPaperScissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.turn = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.new_game = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(28, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 100);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(146, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 100);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(264, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 100);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(28, 134);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 100);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(146, 134);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 100);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(264, 134);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 100);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(28, 240);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 100);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(146, 240);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(111, 100);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(264, 240);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(111, 100);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // turn
+            // 
+            this.turn.AutoSize = true;
+            this.turn.Location = new System.Drawing.Point(452, 108);
+            this.turn.Name = "turn";
+            this.turn.Size = new System.Drawing.Size(151, 20);
+            this.turn.TabIndex = 9;
+            this.turn.Text = "Играч н е на ред";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(417, 108);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 20);
+            this.result.TabIndex = 10;
+            // 
+            // new_game
+            // 
+            this.new_game.Location = new System.Drawing.Point(411, 267);
+            this.new_game.Name = "new_game";
+            this.new_game.Size = new System.Drawing.Size(166, 46);
+            this.new_game.TabIndex = 11;
+            this.new_game.Text = "Нова игра";
+            this.new_game.UseVisualStyleBackColor = true;
+            this.new_game.Click += new System.EventHandler(this.new_game_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 364);
+            this.Controls.Add(this.new_game);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.turn);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Морски шах";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label turn;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button new_game;
+    }
+}
+
---
+++ RockPaperScissors/Form1.cs
@@ -0,0 +1,126 @@
+﻿//Лора Недкова Недева F112913
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class Form1 : Form
+    {
+        private Button[] buttons;//масив, съдържащ всички клетки върху, които ще се играе
+        private string currPlayer = "X";// ред на играча
+        public Form1()
+        {
+            InitializeComponent();
+            buttons = new Button[] { button1, button2, button3, button4, button5, button6,
+            button7, button8, button9};//добавяне на бутоните в масива
+
+            turn.Text = $"Играч {currPlayer} на ход";//текст, показващ кой е на ред
+
+            foreach (Button btn in buttons){//присвояване на клетките за игра метод клик
+                btn.Click += BtnClick;
+            }
+        }
+
+        private void BtnClick(object sender, EventArgs e)// метод за натискане на клетките
+        {
+            Button btn = sender as Button;//кой бутон е натиснат
+            if(btn.Text == "")//ако полето още не е използвано
+            {
+                btn.Text = currPlayer;//маркира се с О или Х
+
+                if (CheckWin())//проверяваме дали има победа
+                {
+                    result.Text = $"Играч {currPlayer} печели!";
+                    turn.Text = "";
+                    DisableBtns();//бутоните стават неактивни след приключване на играта
+                    return;
+                }
+
+                if (IsDraw())//проверяваме равенство
+                {
+                    result.Text = $"Равенство!";
+                    turn.Text = "";
+                    return;
+                }
+
+                currPlayer = currPlayer=="X"?"O":"X";//сегашния играч се променя
+                turn.Text = $"Играч {currPlayer} на ход";
+            }
+        }
+
+        private bool CheckWin()//проверка за победа
+        {
+            int[,] wins = new int[,]//масив с всички възможни победи
+            {
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, //победи редове
+                {0, 4, 8}, {2, 4, 6},            // победи диагонали
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8}  //победи колони
+            };
+
+            for(int i = 0; i < wins.GetLength(0); i++)//обхождане на масива
+            {
+                //променливи, приемащи стойности на възможните комбинации победи
+                int first = wins[i, 0];
+                int second = wins[i, 1];
+                int third = wins[i, 2];
+
+                //проверка дали те съвпадат с изиграните ходове
+                if (buttons[first].Text != "" &&
+                    buttons[first].Text == buttons[second].Text &&
+                    buttons[first].Text == buttons[third].Text)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        private bool IsDraw()//проверка за равенство
+        {
+            foreach(Button b in buttons)//обхождане на всяко поле за игра
+            {
+                if(b.Text == "")
+                {
+                    return false;// ако е празно няма как да има равенство
+                }
+            }
+            //ако всички полета притежават стойност и не е настъпила победа, има равенство
+            return true;
+        }
+
+        private void DisableBtns()//деактивиране на всички полета за игра
+        {
+            foreach(Button b in buttons)
+            {
+                b.Enabled = false;
+            }
+        }
+
+        private void new_game_Click(object sender, EventArgs e)//бутон за нова игра
+        {
+            foreach (Button btn in buttons)//всички полета са празни и могат да се попълнят
+            {
+                btn.Text = "";
+                btn.Enabled = true;
+            }
+
+            currPlayer = "X";//първи играч по подразбиране е Х
+            result.Text = "";
+            turn.Text = $"Играч {currPlayer} на ход";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
